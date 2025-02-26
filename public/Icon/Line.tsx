4955aa8b6ab739923fd9aff8d5ca6eadce92af8a
chasyuss <cotn963@gmail.com>
2025-02-26 23:58:40 +0900

    style: 아이콘 추가

---
+++ public/Icon/Line.tsx
@@ -0,0 +1,20 @@
+const Line = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="202"
+      height="2"
+      viewBox="0 0 202 2"
+      fill="none"
+    >
+      <path
+        d="M1 1H201"
+        stroke="#EBEEF3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Line;
