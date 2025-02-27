bbbfc0a20e531cdacdaaccf1a77de5a4b1fa83a5
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-02-27 16:34:06 +0900

    style: 공통 button 컴포넌트 수정

--- public/Icon/StateDropdown.tsx
+++ public/Icon/StateDropdown.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const StateDropdown = ({ selected }: any) => {
   return (
     <svg
@@ -12,7 +10,7 @@ const StateDropdown = ({ selected }: any) => {
     >
       <path
         d="M5.86603 7.5C5.48113 8.16667 4.51887 8.16667 4.13397 7.5L0.669874 1.5C0.284974 0.833333 0.7661 -8.94676e-07 1.5359 -8.27378e-07L8.4641 -2.21695e-07C9.2339 -1.54397e-07 9.71503 0.833333 9.33013 1.5L5.86603 7.5Z"
-        fill={selected === '선택' ? '#9E9E9E' : '#009856'} 
+        fill={selected === '선택' ? '#9E9E9E' : '#009856'}
       />
     </svg>
   );
