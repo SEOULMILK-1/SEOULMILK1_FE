54ac26cf5f2a0c50c9371d34adff2379944b3bec
chasyuss <cotn963@gmail.com>
2025-02-28 15:23:34 +0900

    style: 본사 고객센터 표 header 구현

---
+++ public/Icon/ChartLine.tsx
@@ -0,0 +1,20 @@
+const ChatLine = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="2"
+      height="14"
+      viewBox="0 0 2 14"
+      fill="none"
+    >
+      <path
+        d="M1 1L0.999999 13"
+        stroke="#DADFE7"
+        stroke-width="2"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+};
+
+export default ChatLine;
