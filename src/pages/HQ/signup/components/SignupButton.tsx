a200a2c71995daadf2446de47da242c9b4f58a86
chasyuss <cotn963@gmail.com>
2025-03-03 18:56:48 +0900

    style: 회원가입 버튼 컴포넌트 구현

--- src/pages/HQ/signup/components/SignupButton.tsx
+++ src/pages/HQ/signup/components/SignupButton.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
   text: string;
+  prevonClick?: () => void;
   onClick?: () => void;
 }
-const SignupButton = ({ text, onClick }: ButtonProps) => {
+const SignupButton = ({ text, onClick, prevonClick }: ButtonProps) => {
   return (
     <div className="flex flex-row pt-8 items-start gap-4 w-full">
-      <button className="flex w-full h-14 px-7 justify-center items-center gap-[10px] rounded-xl border border-solid border-primary-600 bg-white text-primary-600 font-xl-semibold text-center">
+      <button
+        className="flex w-full h-14 px-7 justify-center items-center gap-[10px] rounded-xl border border-solid border-primary-600 bg-white text-primary-600 font-xl-semibold text-center"
+        onClick={prevonClick}
+      >
         이전
       </button>
       <button
