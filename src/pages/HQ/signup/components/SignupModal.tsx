a16426921f128bad718081da6b9b7d1aa37249e0
chasyuss <cotn963@gmail.com>
2025-03-03 19:02:42 +0900

    style: 모달 text 변경

--- src/pages/HQ/signup/components/SignupModal.tsx
+++ src/pages/HQ/signup/components/SignupModal.tsx
@@ -1,6 +1,8 @@
+import { useNavigate } from 'react-router-dom';
 import SignupCheck from '../../../../../public/Icon/SignupCheck';
 
 const SignupModal = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
       <div className="flex flex-col w-[480px] px-8 py-[42px] justify-center items-center rounded-[32px] drop-shadow-elevation1 bg-white">
@@ -16,8 +18,11 @@ const SignupModal = () => {
           가입 승인을 기다려 주세요.
         </span>
 
-        <button className="mt-14 flex w-[200px] h-14 px-7 justify-center items-center rounded-xl border border-solid border-primary-600 bg-white text-primary-600 font-xl-semibold">
-          돌아가기
+        <button
+          className="mt-14 flex w-[200px] h-14 px-7 justify-center items-center rounded-xl border border-solid border-primary-600 bg-white text-primary-600 font-xl-semibold"
+          onClick={() => navigate('/login')}
+        >
+          로그인하기
         </button>
       </div>
     </div>
