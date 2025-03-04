9e87def66c64ac78578687c8ca2fa015436060ac
chasyuss <cotn963@gmail.com>
2025-03-04 22:03:45 +0900

    style: 대리점 회원가입 에러메세지 추가

--- src/pages/CS/cssignup/components/CsSignup2.tsx
+++ src/pages/CS/cssignup/components/CsSignup2.tsx
@@ -15,6 +15,15 @@ interface FormState {
   email: string;
 }
 
+interface Errors {
+  agency: string;
+  bank: string;
+  account: string;
+  phone: string;
+  email: string;
+  [agree: string]: string;
+}
+
 const CsSignup2 = () => {
   const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -26,9 +35,58 @@ const CsSignup2 = () => {
     phone: '',
     email: ''
   });
+  const [errors, setErrors] = useState<Errors>({
+    agency: '',
+    bank: '',
+    account: '',
+    phone: '',
+    email: ''
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const validate = (): boolean => {
+    let newErrors: Errors = {
+      agency: '',
+      bank: '',
+      account: '',
+      phone: '',
+      email: ''
+    };
+    let isValid = true;
+
+    if (!formState.bank.trim()) {
+      newErrors.bank = '지급 요청 계좌를 입력해주세요.';
+      isValid = false;
+    }
+
+    if (!formState.account.trim()) {
+      newErrors.account = '계좌번호를 입력해주세요.';
+      isValid = false;
+    }
+
+    if (!formState.phone.trim()) {
+      newErrors.phone = '전화번호를 입력해주세요';
+      isValid = false;
+    }
+
+    if (!formState.email.trim()) {
+      newErrors.email = '이메일을 입력해주세요';
+      isValid = false;
+    }
+
+    const isAgreeChecked = document.querySelector(
+      'input[type="checkbox"]:checked'
+    );
+    if (!isAgreeChecked) {
+      newErrors['agree'] = '개인정보 수집 및 이용 동의를 해주세요.';
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
   };
 
   return (
@@ -38,60 +96,62 @@ const CsSignup2 = () => {
           <CircleNumber type="two" />
         </div>
 
-        <div className="flex flex-col w-full gap-7 justify-center items-start ">
-          <div className="flex flex-col gap-2 w-full">
-            <label className="text-gray-600 font-md-semibold"> 대리점 </label>
-            <SignupInput
-              name="agency"
-              placeholder="선택"
-              type="text"
-              value={formState.agency}
-              onChange={handleChange}
-            />
-          </div>
+        {/* <div className="flex flex-col w-full gap-7 justify-center items-start "> */}
+        <div className="flex flex-col gap-2 w-full">
+          <label className="text-gray-600 font-md-semibold"> 대리점 </label>
+          <SignupInput
+            name="agency"
+            placeholder="선택"
+            type="text"
+            value={formState.agency}
+            onChange={handleChange}
+          />
+        </div>
 
-          <div className="flex flex-col gap-2 w-full">
-            <label className="text-gray-600 font-md-semibold">
-              사업자 계좌 (지급 요청 계좌)
-            </label>
-            <SignupInput
-              name="bank"
-              placeholder="은행명 입력"
-              type="text"
-              value={formState.bank}
-              onChange={handleChange}
-            />
-            <SignupInput
-              name="account"
-              placeholder="계좌번호 입력"
-              type="text"
-              value={formState.account}
-              onChange={handleChange}
-            />
-          </div>
+        <div className="flex flex-col gap-2 w-full">
+          <label className="text-gray-600 font-md-semibold">
+            사업자 계좌 (지급 요청 계좌)
+          </label>
+          <SignupInput
+            name="bank"
+            placeholder="은행명 입력"
+            type="text"
+            value={formState.bank}
+            onChange={handleChange}
+          />
 
-          <div className="flex flex-col gap-2 w-full">
-            <label className="text-gray-600 font-md-semibold"> 전화번호 </label>
-            <SignupInput
-              name="phone"
-              placeholder="-없이 숫자만 입력"
-              type="text"
-              value={formState.phone}
-              onChange={handleChange}
-            />
-          </div>
+          <SignupInput
+            name="account"
+            placeholder="계좌번호 입력"
+            type="text"
+            value={formState.account}
+            onChange={handleChange}
+          />
+        </div>
 
-          <div className="flex flex-col gap-2 w-full">
-            <label className="text-gray-600 font-md-semibold"> 이메일 </label>
-            <SignupInput
-              name="email"
-              placeholder="example@email.com"
-              type="email"
-              value={formState.email}
-              onChange={handleChange}
-            />
-          </div>
+        <div className="flex flex-col gap-2 w-full">
+          <label className="text-gray-600 font-md-semibold"> 전화번호 </label>
+          <SignupInput
+            name="phone"
+            placeholder="-없이 숫자만 입력"
+            type="text"
+            value={formState.phone}
+            onChange={handleChange}
+          />
+        </div>
 
+        <div className="flex flex-col gap-2 w-full">
+          <label className="text-gray-600 font-md-semibold"> 이메일 </label>
+          <SignupInput
+            name="email"
+            placeholder="example@email.com"
+            type="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex w-full flex-col">
           <div className="flex w-full items-center">
             <div className="relative items-center mt-1">
               <input
@@ -116,12 +176,22 @@ const CsSignup2 = () => {
               </div>
             </div>
           </div>
+          {/* 임시위치 */}
+          {errors['agree'] && (
+            <p className="text-warning-700 font-sm-regular mt-2">
+              {errors['agree']}
+            </p>
+          )}
         </div>
 
         <SignupButton
           text="회원가입"
           prevonClick={() => navigate('/cs/signup')}
-          onClick={() => setIsModalOpen(true)}
+          onClick={() => {
+            if (validate()) {
+              setIsModalOpen(true);
+            }
+          }}
         />
       </div>
       {isModalOpen && <SignupModal />}
