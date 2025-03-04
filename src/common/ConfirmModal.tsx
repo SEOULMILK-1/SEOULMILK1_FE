a71fdc01e25c95c1980b5b60a911c47958150de4
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-03-04 19:24:23 +0900

    style:대리점 세금계산서 UI 구현

--- src/common/ConfirmModal.tsx
+++ src/common/ConfirmModal.tsx
@@ -19,32 +19,32 @@ function ConfirmModal({
 }: ConfirmModalProps) {
   if (!isOpen) return null;
 
-    return (
-      <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
-        <div className="bg-white w-[328px] rounded-[16px] shadow-lg gap-[4px] drop-shadow-elevation2">
-          <p className=" font-xl-semibold mt-[40px] text-center text-gray-800">
-            {title}
-          </p>
-          <p className="text-gray-500 font-md-medium mt-[4px] mb-[40px] text-center">
-            {description}
-          </p>
-          <div className="flex">
-            <button
-              className="w-[164px] h-[56px] bg-gray-200 text-gray-600 rounded-bl-[16px] font-md-semibold"
-              onClick={onClose}
-            >
-              {cancelText}
-            </button>
-            <button
-              className="w-[164px] h-[56px] bg-warning-400 text-white rounded-br-[16px] font-md-semibold"
-              onClick={onConfirm}
-            >
-              {confirmText}
-            </button>
-          </div>
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
+      <div className="bg-white w-[328px] rounded-[16px] shadow-lg gap-[4px] drop-shadow-elevation2">
+        <p className=" font-xl-semibold mt-[40px] text-center text-gray-800">
+          {title}
+        </p>
+        <p className="text-gray-500 font-md-medium mt-[4px] mb-[40px] text-center">
+          {description}
+        </p>
+        <div className="flex">
+          <button
+            className="w-[164px] h-[56px] bg-gray-200 text-gray-600 rounded-bl-[16px] font-md-semibold"
+            onClick={onClose}
+          >
+            {cancelText}
+          </button>
+          <button
+            className="w-[164px] h-[56px] bg-warning-400 text-white rounded-br-[16px] font-md-semibold"
+            onClick={onConfirm}
+          >
+            {confirmText}
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
 export default ConfirmModal;
