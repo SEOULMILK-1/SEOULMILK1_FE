b503d5f013a83ad9e562ecee283af98e5e913c61
chasyuss <cotn963@gmail.com>
2025-03-05 16:35:19 +0900

    style: 표 이름 수정

--- src/pages/ADMIN/user/components/UserDataTable.tsx
+++ src/pages/ADMIN/user/components/UserDataTable.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import UserSideModal from './UserSideModal';
 import ChartPagination from '../../../../common/ChartPagination';
 import UserDataTableHeader from './UserDataTableHeader';
-// import PlusIcon from '../../../../../public/Icon/PlusIcon';
+import PlusIcon from '../../../../../public/Icon/PlusIcon';
 import api from '../../../../hooks/api';
 
 interface User {
@@ -11,8 +11,9 @@ interface User {
   name: string;
   phone: string;
   role: 'ADMIN';
-  team: string;
+  csName: string;
   createdAt: string;
+  isAssigned: string;
 }
 
 const UserDataTable = () => {
@@ -26,7 +27,7 @@ const UserDataTable = () => {
         const token = localStorage.getItem('accesstoken');
         console.log('accesstoken', token);
 
-        const response = await api.get('/admin/pending', {
+        const response = await api.get('/admin/user', {
           headers: {
             Authorization: `Bearer ${token}`
           }
@@ -61,27 +62,31 @@ const UserDataTable = () => {
             className="flex w-[932px] h-[42px] items-center hover:bg-gray-100 rounded-xl cursor-pointer"
             onClick={() => openModal(row)}
           >
-            {/* <div
+            {/* status */}
+            <div
               className={`w-[92px] pl-5 font-sm-medium ${
-                row.status === '미등록' ? 'text-warning-600' : 'text-gray-800'
+                row.isAssigned === '미등록'
+                  ? 'text-warning-600'
+                  : 'text-gray-800'
               }`}
             >
-              {row.status}
-            </div> */}
-            <div className="w-[120px] pl-5 gap-5 text-gray-800 font-sm-medium">
+              {row.isAssigned}
+            </div>
+
+            <div className="w-[190px] pl-5 gap-5 text-gray-800 font-sm-medium">
+              {row.csName}
+            </div>
+            <div className="w-[120px] pl-5 ml-1 text-gray-800 font-sm-medium">
               {row.name}
             </div>
-            <div className="w-[190px] pl-5 ml-1 text-gray-800 font-sm-medium">
-              {row.team}
-            </div>
-            <div className="w-[200px] pl-5 text-gray-800 font-sm-medium">
+            <div className="w-[200px] pl-5 text-gray-800 font-sm-medium text-left">
               {row.phone}
             </div>
             <div className="w-[170px] pl-5 ml-1 text-gray-800 font-sm-medium">
               {row.createdAt}
             </div>
-            {/* <div className="w-[140px] px-5 py-2 items-center">
-              {row.status === '미등록' && (
+            <div className="w-[140px] px-5 py-2 items-center">
+              {row.isAssigned === '미등록' && (
                 <button
                   className="flex w-[85px] h-[26px] gap-1 pl-2 pr-3 items-center justify-center rounded-lg bg-[#E6F1F7] text-[#2C72FF] font-xs-regular"
                   onClick={(e) => {
@@ -93,7 +98,7 @@ const UserDataTable = () => {
                   등록하기
                 </button>
               )}
-            </div> */}
+            </div>
           </div>
         ))}
       </div>
