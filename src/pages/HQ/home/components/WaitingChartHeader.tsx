21bdd213b2a5ba749a8b6e23e5158fa014535f16
chasyuss <cotn963@gmail.com>
2025-03-06 23:45:25 +0900

    style: 지급 대기 세금계산서 ui 구현

---
+++ src/pages/HQ/home/components/WaitingChartHeader.tsx
@@ -0,0 +1,25 @@
+import ChartLine from '../../../../../public/Icon/ChartLine';
+
+const WaitingChartHeader = () => {
+  return (
+    <div className="flex w-[960px] h-14 pl-2 pr-5 items-center border-b border-gray-300">
+      <div className="w-[200px] h-[42px] pl-5 mt-6 text-gray-500 font-sm-medium">
+        대리점
+      </div>
+      <div className="mt-1">
+        <ChartLine />
+      </div>
+      <div className="w-[562px] h-[42px] pl-5 mt-6 text-gray-500 font-sm-medium">
+        제목
+      </div>
+      <div className="mt-1">
+        <ChartLine />
+      </div>
+      <div className="w-[170px] h-[42px] pl-5 mt-6 text-gray-500 font-sm-medium">
+        발행일
+      </div>
+    </div>
+  );
+};
+
+export default WaitingChartHeader;
