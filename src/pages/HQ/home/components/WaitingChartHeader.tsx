292ae673e008cea12860ca8647e26114ab2c786a
chasyuss <cotn963@gmail.com>
2025-03-11 22:43:44 +0900

    fix: 칼럼 이름 수정

--- src/pages/HQ/home/components/WaitingChartHeader.tsx
+++ src/pages/HQ/home/components/WaitingChartHeader.tsx
@@ -22,7 +22,7 @@ const WaitingChartHeader = () => {
         <ChartLine />
       </div>
       <div className="w-[120px] h-[42px] pl-5 mt-6 text-gray-500 font-sm-medium">
-        대리점 관리자
+        대리점 담당자
       </div>
     </div>
   );
