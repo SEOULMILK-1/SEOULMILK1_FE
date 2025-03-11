5f3aab24e0a676c28b51e716c0f03f1059a9c188
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-03-12 01:14:39 +0900

    fix: 로그인 후 라우터 수정

--- src/pages/HQ/payment/components/PaymentContent.tsx
+++ src/pages/HQ/payment/components/PaymentContent.tsx
@@ -21,9 +21,7 @@ const PaymentChartContent = ({ data }: Props) => {
         <div
           key={item.paymentResolutionId}
           className="mx-[8px] flex w-[960px] h-[42px] items-center rounded-[12px] hover:bg-gray-100 font-sm-medium overflow-x-hidden"
-          onClick={() =>
-            navigate(`/payment/detail/${item.paymentResolutionId}`)
-          }
+          onClick={() => navigate(`detail/${item.paymentResolutionId}`)}
         >
           <div className="w-[200px] pl-5 text-sm font-medium text-gray-700 truncate">
             {item.suDeptName}
