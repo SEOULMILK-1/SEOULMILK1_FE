81b20ff5e2269e9d77ac7dadf1daeafe7f6a508c
chasyuss <cotn963@gmail.com>
2025-03-13 09:17:44 +0900

    fix: admin 대리점 소속기관으로 변경

--- src/pages/ADMIN/home/components/DashTableHeader.tsx
+++ src/pages/ADMIN/home/components/DashTableHeader.tsx
@@ -13,7 +13,7 @@ const DashTableHeader = () => {
         <ChartLine />
       </div>
       <div className="w-[298px] h-[42px] pl-5 mt-6 text-gray-500 font-sm-medium">
-        대리점
+        소속기관
       </div>
       <div className="mt-1">
         <ChartLine />
