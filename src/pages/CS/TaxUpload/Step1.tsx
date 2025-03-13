0bbac45590b9ad90a01447d125d580eeb9ce045d
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2025-03-13 17:24:49 +0900

    fix: 탑 네비게이션, 편집박스 재정렬

--- src/pages/CS/TaxUpload/Step1.tsx
+++ src/pages/CS/TaxUpload/Step1.tsx
@@ -123,7 +123,6 @@ const Step1 = () => {
           <img src={uploadIcon} alt="세금계산서 업로드" className="w-6 h-6" />
         )}
       />
-
       {/* 처음 사진업로드 확인모달 */}
       {isModalOpen && (
         <ConfirmUpload
@@ -132,7 +131,6 @@ const Step1 = () => {
           onClose={() => setIsModalOpen(false)}
         />
       )}
-
       {/* 중복일경우 모달 */}
       {isDuplicateModalOpen && (
         <DuplicateTaxModal
@@ -142,13 +140,12 @@ const Step1 = () => {
           onClose={() => setIsDuplicateModalOpen(false)}
         />
       )}
-
-      <ImageCrop
-        initialImage={selectedImage}
-        onCropComplete={(croppedImg) => {
-          setCroppedImage(croppedImg ?? selectedImage ?? null);
-        }}
-      />
+      <div className="mr-20">
+        <ImageCrop
+          initialImage={selectedImage}
+          onCropComplete={(croppedImg) => setCroppedImage(croppedImg)}
+        />
+      </div>
       <input
         type="file"
         accept="image/*"
